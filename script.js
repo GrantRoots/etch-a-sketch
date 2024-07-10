867811f492bb544cc294b76360b5c479a3dab606
GrantRoots <158632641+GrantRoots@users.noreply.github.com>
2024-07-09 21:31:08 -0700

    Make New Grid button and Fix square sizing
    Made the New Grid button that removes all the old squares, prompts the user for a number, then makes a new grid with that number of squares as the sides.
    
    Made the squares a 1 to 1 ratio with the width.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('#container')
 
-for (let i = 0; i < 16; i++){
+for (let i = 0; i < 16; i++) {
     for (let n = 0; n < 16; n++) {
         const square = document.createElement('div')
         square.classList.add('square')
@@ -14,5 +14,28 @@ const square = document.querySelectorAll('.square').forEach(square => square.add
 
 const button = document.querySelector('button')
 button.addEventListener('click', () => {
-    let input = prompt('How many squares')
+    let input = prompt('How many squares per side?')
+    while (input > 100 || input < 1) {
+        input = prompt('How many squares per side? (100 or less)')
+    }
+    while (container.firstChild) {
+        container.firstChild.remove()
+    }
+
+    for (let i = 0; i < input; i++) {
+        for (let n = 0; n < input; n++) {
+            const square = document.createElement('div')
+            square.classList.add('square')
+            container.appendChild(square)
+        }
+    }
+    
+    let newSquares = document.getElementsByClassName('square')
+    for (let x = 0; x < newSquares.length; x++) {
+        newSquares[x].style['flex-basis'] = (100/input) + '%'
+    }
+
+    const square = document.querySelectorAll('.square').forEach(square => square.addEventListener('mouseenter', () => {
+        square.style['background-color'] = 'red'
+    }))
 })
