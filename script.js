f6efb926599729dedfb29d0251f4a4fe22d77fe4
GrantRoots <158632641+GrantRoots@users.noreply.github.com>
2024-07-09 20:09:37 -0700

    Make squares change color
    Made it so the squres change color when mouse enters them.

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let container = document.querySelector('#container')
+const container = document.querySelector('#container')
 
 for (let i = 0; i < 16; i++){
     for (let n = 0; n < 16; n++) {
-        let square = document.createElement('div')
+        const square = document.createElement('div')
         square.classList.add('square')
         container.appendChild(square)
     }
 }
+
+const square = document.querySelectorAll('.square').forEach(square => square.addEventListener('mouseenter', () => {
+    square.style['background-color'] = 'red'
+}))
+
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+    let input = prompt('How many squares')
+})
